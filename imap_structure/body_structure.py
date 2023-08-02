cb1c81e385345e1829e05e2535adca4f9ee09eec
百地 希留耶 <65301509+KiruyaMomochi@users.noreply.github.com>
2023-08-02 20:20:21 +0800

    Support extract info from imapclient

--- imap_structure/body_structure.py
+++ imap_structure/body_structure.py
@@ -1,3 +1,5 @@
+import collections.abc
+import logging
 from dataclasses import dataclass
 from email import _header_value_parser as parser
 from functools import cached_property
@@ -5,7 +7,6 @@ from typing import NamedTuple, Optional
 
 from pyparsing import (
     Literal,
-    ParseResults,
     Word,
     alphanums,
     nested_expr,
@@ -14,6 +15,8 @@ from pyparsing import (
     remove_quotes,
     replace_with,
 )
+
+logger = logging.getLogger(__name__)
 
 
 def get_bs_parser():
@@ -38,7 +41,7 @@ bodystructure_parser = get_bs_parser()
 
 class MimeTree(NamedTuple):
     mime_type: list[str]
-    children: list[ParseResults]
+    children: list[collections.abc.Sequence]
 
 
 @dataclass
@@ -55,7 +58,7 @@ class BodyPartNonMultipart(BodyPart):
 
     body_type: str
     body_subtype: str
-    body_parameters: ParseResults
+    body_parameters: collections.abc.Sequence
     body_id: Optional[str]
     body_description: Optional[str]
     body_encoding: str
@@ -64,32 +67,36 @@ class BodyPartNonMultipart(BodyPart):
     # Type-specific fields
     # https://datatracker.ietf.org/doc/html/rfc9051#section-7.5.2-4.32.2
 
-    message_rfc822_envelope: Optional[ParseResults] = None
+    message_rfc822_envelope: Optional[collections.abc.Sequence] = None
     text_lines: Optional[int] = None
 
     # Extension fields
     # https://datatracker.ietf.org/doc/html/rfc9051#section-7.5.2-4.32.5
 
     body_md5: Optional[str] = None
-    body_disposition: Optional[ParseResults] = None
-    body_language: Optional[str | ParseResults] = None
+    body_disposition: Optional[collections.abc.Sequence] = None
+    body_language: Optional[str | collections.abc.Sequence] = None
     body_location: Optional[str] = None
 
 
 @dataclass
 class BodyPartMultipart(BodyPart):
-    body_parameters: Optional[ParseResults] = None
-    body_disposition: Optional[ParseResults] = None
-    body_language: Optional[str | ParseResults] = None
+    body_parameters: Optional[collections.abc.Sequence] = None
+    body_disposition: Optional[collections.abc.Sequence] = None
+    body_language: Optional[str | collections.abc.Sequence] = None
     body_location: Optional[str] = None
 
 
 class BodyStructure:
     _raw: Optional[str]
-    parse_results: ParseResults
+    parse_results: collections.abc.Sequence
 
-    def __init__(self, parse_results: ParseResults, raw: Optional[str] = None):
+    def __init__(
+        self, parse_results: collections.abc.Sequence, raw: Optional[str] = None
+    ):
         self._raw = raw
+        if len(parse_results) == 0:
+            raise ValueError("parse_results must not be empty")
         self.parse_results = parse_results
 
     @cached_property
@@ -110,10 +117,12 @@ class BodyStructure:
                 continue
             if part.body_disposition is None:
                 continue
-            disposition = part.body_disposition.as_list()
+            disposition = part.body_disposition
             if "attachment" not in disposition:
                 continue
-            if len(disposition) > 1 and isinstance(disposition[1], list):
+            if len(disposition) > 1 and isinstance(
+                disposition[1], collections.abc.Sequence
+            ):
                 filename_idx = disposition[1].index("filename")
                 if len(disposition[1]) > filename_idx + 1:
                     filename = disposition[1][filename_idx + 1]
@@ -125,7 +134,7 @@ class BodyStructure:
         return any(
             isinstance(part, BodyPartNonMultipart)
             and part.body_disposition is not None
-            and "attachment" in part.body_disposition.as_list()
+            and "attachment" in part.body_disposition
             for part in self.body_part.children
         )
 
@@ -138,17 +147,19 @@ def parse_body_structure(body_structure: str) -> BodyStructure:
     """
     # parse it
     parsed_structure = bodystructure_parser.parse_string(body_structure)
+    parsed_structure = parsed_structure.as_list()
+    # as we actually use the parse result like if it's list
+    # we will convert it to nested list to avoid complicated ducktype check
 
     assert len(parsed_structure) == 1
     parsed_structure = parsed_structure[0]
 
-    assert isinstance(parsed_structure, ParseResults)
-
+    assert isinstance(parsed_structure, collections.abc.Sequence)
     return BodyStructure(parsed_structure, raw=body_structure)
 
 
 def extract_mime_tree(
-    parsed_structure: ParseResults,
+    parsed_structure: collections.abc.Sequence,
 ) -> MimeTree:
     """
     Extract body structure information from a parsed result.
@@ -165,6 +176,14 @@ def extract_mime_tree(
                 # https://www.rfc-editor.org/rfc/rfc9051.html#section-7.5.2-4.10.7
         return (parsed_structure[:1], [])  # type: ignore
 
+    # For result from imapclient, the parts before MIME type is already collected as a list
+    if isinstance(parsed_structure, tuple) and isinstance(parsed_structure[0], list):
+        mime = parsed_structure[1]
+        if isinstance(mime, str):
+            return MimeTree([mime], parsed_structure[0])
+        if isinstance(mime, bytes):
+            raise NotImplementedError("all bytes should be decoded before parsing")
+    # For result from email.message_from_bytes, the parts before MIME type is flattened with rest of the structure
     result = []
     for part in parsed_structure:
         if isinstance(part, str):
@@ -185,7 +204,7 @@ def child_section(prefix: str, index: int):
 
 
 def extract_non_multipart(
-    section: str, mime: list[str], parsed_structure: ParseResults
+    section: str, mime: list[str], parsed_structure: collections.abc.Sequence
 ):
     index = 0
 
@@ -194,7 +213,7 @@ def extract_non_multipart(
     index += 2
 
     body_parameters = parsed_structure[index]
-    assert isinstance(body_parameters, ParseResults)
+    assert isinstance(body_parameters, collections.abc.Sequence)
     index += 1
 
     body_id = parsed_structure[index]
@@ -270,12 +289,12 @@ def extract_multipart(
     section: str,
     mime: list[str],
     children: list[BodyPart],
-    parsed_structure: ParseResults,
+    parsed_structure: collections.abc.Sequence,
 ) -> BodyPartMultipart:
     """
     Generate a tree for the body part of the message
     """
-    li = parsed_structure.as_list()
+    li = parsed_structure
     index = li.index(mime[-1])
     assert index > 0
 
@@ -289,21 +308,28 @@ def extract_multipart(
         return result
 
     body_parameters = parsed_structure[index]
-    assert isinstance(body_parameters, ParseResults)
+    assert isinstance(body_parameters, collections.abc.Sequence)
     result.body_parameters = body_parameters
     index += 1
     if len(parsed_structure) <= index:
         return result
 
     body_disposition = parsed_structure[index]
-    assert isinstance(body_disposition, ParseResults)
+    assert (
+        isinstance(body_disposition, collections.abc.Sequence)
+        or body_disposition is None
+    )
     result.body_disposition = body_disposition
     index += 1
     if len(parsed_structure) <= index:
         return result
 
     body_language = parsed_structure[index]
-    assert isinstance(body_language, ParseResults) or isinstance(body_language, str)
+    assert (
+        isinstance(body_language, collections.abc.Sequence)
+        or isinstance(body_language, str)
+        or body_language is None
+    )
     result.body_language = body_language
     index += 1
     if len(parsed_structure) <= index:
@@ -317,7 +343,9 @@ def extract_multipart(
     return result
 
 
-def extract_body_part(parsed_structure: ParseResults, section: str = "") -> BodyPart:
+def extract_body_part(
+    parsed_structure: collections.abc.Sequence, section: str = ""
+) -> BodyPart:
     """
     Generate a tree for the body part of the message
     """
@@ -326,6 +354,7 @@ def extract_body_part(parsed_structure: ParseResults, section: str = "") -> Body
         try:
             return extract_non_multipart(section, mime, parsed_structure)
         except:
+            logger.exception("Failed to extract non-multipart")
             return BodyPart(section, mime, [])
 
     children = [
@@ -336,18 +365,19 @@ def extract_body_part(parsed_structure: ParseResults, section: str = "") -> Body
     try:
         return extract_multipart(section, mime, children, parsed_structure)
     except:
+        logger.exception("Failed to extract multipart")
         return BodyPart(section, mime, children)
 
 
-def extract_text_sections(parsed_structure: BodyPart) -> list[tuple[str, str]]:
+def extract_text_sections(body_part: BodyPart) -> list[tuple[str, str]]:
     """
     Select text sections from given MIME structure
 
     :returns: a list of (section, text MIME subtype) tuples
     """
-    section = parsed_structure.section
-    mime = parsed_structure.mime_type
-    children = parsed_structure.children
+    section = body_part.section
+    mime = body_part.mime_type
+    children = body_part.children
 
     if mime[0] == "TEXT":
         return [(section, mime[1])]
